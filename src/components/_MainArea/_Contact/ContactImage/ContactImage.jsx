0b1d5f6fa7d5dc557088f3c3637f492cccebd402
cancion97 <mikiasm1207@gmail.com>
2025-11-19 16:53:06 +0900

    005

--- src/components/_MainArea/_Contact/ContactImage/ContactImage.jsx
+++ src/components/_MainArea/_Contact/ContactImage/ContactImage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import take from "./../../../../assets/take.png";
+import styles from "./ContactImage.module.css";
 
 export default function ContactImage() {
   return (
     <div>
-      <img src={take} alt="猫の写真" />
+      <img className={styles.image} src={take} alt="猫の写真" />
     </div>
   );
 }
