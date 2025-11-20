caec5a36f8b449756eb27db5f3565f0e88b25977
cancion97 <mikiasm1207@gmail.com>
2025-11-20 10:37:05 +0900

    010

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Header from "./components/_Header/Header/Header";
-import Navigation from "./components/_Header/Navigation/Navigation";
+
 import MainArea from "./components/_MainArea/MainArea/MainArea";
 
 function App() {
