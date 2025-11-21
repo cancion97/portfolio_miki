3180bd54c724454834ba433b37aef9d456bd3685
Asami Miki <mikiasm1207@gmail.com>
2025-11-21 15:04:28 +0900

    017

--- src/components/_MainArea/_Works/WorkItem/WorkItem.jsx
+++ src/components/_MainArea/_Works/WorkItem/WorkItem.jsx
@@ -5,19 +5,33 @@ import arrow from "../../../../assets/arrow.png";
 import styles from "./WorkItem.module.css";
 import clip from "../../../../assets/clip.png";
 import close from "../../../../assets/close.png";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Construction from "../../../Construction/Construction";
 
 export default function WorkItem() {
   const [isOpen, setIsOpen] = useState(false);
 
+  // モーダル中の背景スクロール禁止
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isOpen]);
+
   const openModal = () => {
-    setIsOpen(!isOpen);
+    setIsOpen(true);
   };
 
   const closeModal = () => {
     setIsOpen(false);
   };
+
   return (
     <>
       {works.map((item) => (
@@ -26,8 +40,10 @@ export default function WorkItem() {
             <div className={styles.clip}>
               <img src={clip} alt="クリップの写真" />
             </div>
+
             <div className={styles.titlewrap}>
               <h3>{item.label}</h3>
+
               <div className={styles.imgbox}>
                 {item.tool.map((icon, index) => (
                   <img
@@ -39,42 +55,41 @@ export default function WorkItem() {
                 ))}
               </div>
             </div>
+
             <p style={{ borderLeft: item.style }} className={styles.text}>
               {item.text.map((line, i) => (
                 <span key={i} className={styles.blockSpan}>
                   {line}
                 </span>
-                // .blockSpanにCSSでdisplay: block;を指定してください。
               ))}
             </p>
+
             <div className={styles.btnbox}>
               <Button btext="サイトにアクセス" icon={link} href={item.url} />
               <Button btext="詳しく見る" icon={arrow} onclick={openModal} />
             </div>
           </div>
-          <div className="modalWrapper">
-            {isOpen && (
-              // modalOverlayはモーダルが出た時の背景
-              <div className={styles.modalOverlay} onClick={closeModal}>
-                {/* モーダルの表示される画面 */}
-                <div
-                  className={styles.modalContent}
-                  onClick={(e) => e.stopPropagation()}
-                >
-                  {/* モーダルとして表示したい内容 */}
-                  <div className={styles.modal}>
-                    <Construction />
-                    <Button
-                      style={styles.btn}
-                      btext="閉じる"
-                      icon={close}
-                      onclick={closeModal}
-                    />
-                  </div>
+
+          {/* モーダル */}
+          {isOpen && (
+            <div className={styles.modalOverlay} onClick={closeModal}>
+              <div
+                className={styles.modalContent}
+                onClick={(e) => e.stopPropagation()}
+              >
+                <div className={styles.modal}>
+                  <Construction />
+                  <Button
+                    style={styles.btn}
+                    btext="閉じる"
+                    icon={close}
+                    onclick={closeModal}
+                  />
                 </div>
               </div>
-            )}
-          </div>
+            </div>
+          )}
+
           <div className={styles.scshots}>
             <img
               className={styles.img}
