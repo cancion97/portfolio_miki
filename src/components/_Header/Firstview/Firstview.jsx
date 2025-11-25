10973aabcadec90250ce8cc12912bf77e988b9b4
cancion97 <mikiasm1207@gmail.com>
2025-11-25 15:31:44 +0900

    023

--- src/components/_Header/Firstview/Firstview.jsx
+++ src/components/_Header/Firstview/Firstview.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import cat from "./../../../assets/firstcat.png";
 import styles from "./Firstview.module.css";
+import beem from "./../../../assets/firstbeem.png";
 
 export default function Firstview() {
   return (
-    <div className={styles.img}>
-      <img src={cat} alt="猫" />
-    </div>
+    <>
+      <div className={styles.wrapper}>
+        <img className={styles.img1} src={cat} alt="猫" />
+        <img className={styles.img2} src={beem} alt="猫の目からビーム" />
+      </div>
+    </>
   );
 }
